3c6a130bd1c4ab06e4f5d16293676e3698ea5521
Marichelle Bisuna <marichellebisuna@yahoo.com>
2020-10-26 19:32:10 +1100

    Navbar and Topbar created

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import Topbar from "./Navbar/Topbar"
 import Header from "./Navbar/Header"
-import "bootstrap/dist/css/bootstrap.min.css"
+import "../assets/vendor/bootstrap/css/bootstrap.min.css"
 import "./layout.css"
 import "../assets/vendor/icofont/icofont.min.css"
 
