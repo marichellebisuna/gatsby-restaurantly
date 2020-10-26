723fa6c7de170e4fcc067864735e32d81b7bed01
Marichelle Bisuna <marichellebisuna@yahoo.com>
2020-10-26 19:52:10 +1100

    error checked

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,9 +2,6 @@ import React from "react"
 import PropTypes from "prop-types"
 import Topbar from "./Navbar/Topbar"
 import Header from "./Navbar/Header"
-import "../assets/vendor/bootstrap/css/bootstrap.min.css"
-import "./layout.css"
-import "../assets/vendor/icofont/icofont.min.css"
 
 const Layout = ({ children }) => {
   return (
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,9 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import "../assets/vendor/bootstrap/css/bootstrap.min.css"
+import "./layout.css"
+import "../assets/vendor/icofont/icofont.min.css"
 
 const IndexPage = () => {
   return (
